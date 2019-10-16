141146425ac56f8233ce02c338e9318b99cb80c1
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-15 21:44:09 -0500

    inserted data into columns

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert burgers 
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lamb Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false);
