8572e8deb3c4c06f54c98d6125a3724b248974a4
Angelica Zamer <angelica.zamer@gmail.com>
2019-10-17 09:43:37 -0500

    added to seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
 -- Insert burgers 
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Lamb Burger", true);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false);mysq
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Jalapeno Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beyond Burger", false);
